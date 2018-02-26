fixed some scaling issues in predictions

JointModel.py
@@ -240,15 +240,13 @@ class JointModel(DisProgBuilder.DPMInterface):
     # newXs are assumed to be already scaled and in the right space
     # not a longitudinal list
 
-    assert self.disModels[disNr].minX <= np.min(newXs)
-    assert self.disModels[disNr].maxX >= np.max(newXs)
 
     # first predict the dysfunctionality scores in the disease specific model
-    dysfuncPredXU = self.disModels[disNr].predictBiomk(newX)
+    dysfuncPredXU = self.disModels[disNr].predictBiomkAndScale(newXs)
 
 
     # then predict the inidividual biomarkers in the disease agnostic models
-    biomkPredXB = np.zeros((newXs, self.nrBiomk))
+    biomkPredXB = np.zeros((newXs.shape[0], self.nrBiomk))
     for u in range(self.nrFuncUnits):
 
       biomkPredXB[:, self.mapBiomkToFuncUnits == u] = \

MarcoModel.py
@@ -1075,7 +1075,11 @@ class GP_progression_model(object):
     def predictBiomkAndScale(self, newX):
       ''' performs scaling of Xs before prediction and Ys after prediction '''
 
-      xsScaled = self.applyScalingXForward(newX, biomk=0)
+      xsScaled = self.applyScalingXForward(newX.reshape(-1,1), biomk=0)
+
+      assert self.minX <= np.min(xsScaled)
+      assert self.maxX >= np.max(xsScaled)
+
       ys = self.predictBiomk(xsScaled)
       ysScaled = self.applyScalingYAllBiomk(ys)
 